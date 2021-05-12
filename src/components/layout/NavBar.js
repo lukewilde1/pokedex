5bff240060b36f6787a482f4283dccffb5ec4852
Luke Wilde <lukewilde98@gmail.com>
2021-05-12 09:13:22 -0700

    Added new font

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -5,7 +5,9 @@ export default class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-light bg-light justify-content-between fixed-top" >
-                <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="javascript:history.back()">Pokdex</a>
+                <a className="navbar-brand col-sm-3 col-md-2 mr-0" href="javascript:history.back()">
+                    <font face="Pokemon" >PoKéDeX</font>
+                </a>
                 <form className="form-inline">
                     <input className="form-control mr-sm-2" type="search" placeholder="Pokemon Name" aria-label="Search"/>
                     <button className="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
